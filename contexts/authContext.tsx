e5d105fd0d37fa3a124ab293c54e744e930a1024
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-06-19 23:59:18 +0500

    upda

--- contexts/authContext.tsx
+++ contexts/authContext.tsx
@@ -1,6 +1,7 @@
-import { auth } from "@/config/firebase";
+import { auth, firestore } from "@/config/firebase";
 import { AuthContextType, UserType } from "@/types";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 import React, { createContext, useState } from "react";
 
 const authContext = createContext<AuthContextType | null>(null);
@@ -28,7 +29,12 @@ export const authProvider: React.FC<{ children: React.ReactNode }> = ({
   const register = async (email: string, password: string) => {
     try {
       // Registering User
-        const response =await  createUserWithEmailAndPassword(auth)
+        const response =await  createUserWithEmailAndPassword(auth, email, password);
+        await setDoc(doc(firestore, 'users', response?.user?.uid), {
+          name,
+          email,
+          uid: response?.user?.uid
+        })
       return { success: true };
     } catch (error: any) {
       let message = error.message;
@@ -38,4 +44,12 @@ export const authProvider: React.FC<{ children: React.ReactNode }> = ({
       };
     }
   };
+
+  const updateUserData = async (uid: string) => {
+    try {
+     const response = await crea
+    } catch (error) {
+      
+    }
+  }
 };
