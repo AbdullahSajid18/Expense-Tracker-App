f9816e3325f551c52c4d79164dbc956eb3c2b442
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-06-25 20:55:42 +0500

    implement cloudinary api

--- services/imageService.ts
+++ services/imageService.ts
@@ -1,4 +1,21 @@
+import { CLOUDINARY_CLOUD_NAME } from "@/constants";
+import { ResponseType } from "@/types";
 
+const CLOUDINARY_CLOUD_URL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`
+
+export const uploadFileToCloudinary = async (
+    file: {uri?: string} | string,
+    folderName: string
+
+): Promise<ResponseType> => {
+    try {
+        return {success: true}
+    } catch (error: any) {
+        console.log("Got Error Uploading File ", error);
+        return {success: false, message: error.message || 'Could Not Upload file'}
+        
+    }
+}
 
 export const getProfileImage = (file: any) => {
     if(file && typeof file == 'string') return file;
