0421f86146957e44704c88e8e806d7fa69d2ef36
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-06-30 15:38:17 +0500

    implement dummy data in Transaction List

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,61 +1,80 @@
-import { ScrollView, StyleSheet, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import Typo from '@/components/Typo'
-import { colors, spacingX, spacingY } from '@/constants/theme'
-import { useAuth } from '@/contexts/authContext'
-import ScreenWrapper from '@/components/ScreenWrapper'
-import { verticalScale } from '@/utils/styling'
-import * as Icons from 'phosphor-react-native';
-import HomeCard from '@/components/HomeCard'
-import TransactionList from '@/components/TransactionList'
+import Button from "@/components/Button";
+import HomeCard from "@/components/HomeCard";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import TransactionList from "@/components/TransactionList";
+import Typo from "@/components/Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { useAuth } from "@/contexts/authContext";
+import { verticalScale } from "@/utils/styling";
+import * as Icons from "phosphor-react-native";
+import React from "react";
+import { ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
 
 const Home = () => {
-  const {user} = useAuth();
+  const { user } = useAuth();
 
- 
   return (
     <ScreenWrapper>
       <View style={styles.container}>
         <View style={styles.header}>
-          <View style={{gap: 4}}>
-            <Typo size={16}  color={colors.neutral400}>Hello,</Typo>
-            <Typo size={20}  fontWeight={500}>{user?.name}</Typo>
-
+          <View style={{ gap: 4 }}>
+            <Typo size={16} color={colors.neutral400}>
+              Hello,
+            </Typo>
+            <Typo size={20} fontWeight={500}>
+              {user?.name}
+            </Typo>
           </View>
           <TouchableOpacity style={styles.searchIcon}>
-            <Icons.MagnifyingGlass 
+            <Icons.MagnifyingGlass
               size={verticalScale(22)}
               color={colors.neutral200}
-              weight='bold'
+              weight="bold"
             />
           </TouchableOpacity>
         </View>
-        <ScrollView contentContainerStyle={styles.scrollViewStyle} showsVerticalScrollIndicator={false}>
+        <ScrollView
+          contentContainerStyle={styles.scrollViewStyle}
+          showsVerticalScrollIndicator={false}
+        >
           {/* card */}
           <View>
             <HomeCard />
           </View>
 
-          <TransactionList data={[1, 2, 3]} title='Recent Transactions' />
-
+          <TransactionList
+            data={[1, 2, 3,4 , 5, 6,]}
+            loading={false}
+            emptyListMessage="No recent transactions found!"
+            title="Recent Transactions"
+          />
         </ScrollView>
+
+
+        <Button style={styles.floatingButton}>
+          <Icons.Plus 
+            color={colors.black}
+            weight="bold"
+            size={verticalScale(24)}
+          />
+        </Button>
       </View>
     </ScreenWrapper>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: spacingX._20,
-    marginTop: verticalScale(8)
+    marginTop: verticalScale(8),
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: spacingY._10,
   },
   searchIcon: {
@@ -67,14 +86,13 @@ const styles = StyleSheet.create({
     height: verticalScale(50),
     width: verticalScale(50),
     borderRadius: 100,
-    position: 'absolute',
+    position: "absolute",
     bottom: verticalScale(30),
-    right: verticalScale(30)
+    right: verticalScale(30),
   },
   scrollViewStyle: {
     marginTop: spacingY._10,
     paddingBottom: verticalScale(100),
-    gap: spacingY._25
-  }
-
-})
+    gap: spacingY._25,
+  },
+});
--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -1,10 +1,12 @@
-
+import Loading from "@/components/Loading";
 import Typo from "@/components/Typo";
+import { expenseCategories } from "@/constants/data";
 import { colors, radius, spacingX, spacingY } from "@/constants/theme";
 import { TransactionItemProps, TransactionListType } from "@/types";
 import { verticalScale } from "@/utils/styling";
 import { FlashList } from "@shopify/flash-list";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
 
 const TransactionList = ({
   data,
@@ -12,11 +14,7 @@ const TransactionList = ({
   loading,
   emptyListMessage,
 }: TransactionListType) => {
-
-  const handleClick = () => {
-
-  }
-
+  const handleClick = () => {};
 
   return (
     <View style={styles.container}>
@@ -29,27 +27,75 @@ const TransactionList = ({
       <View style={styles.list}>
         <FlashList
           data={data}
-          renderItem={({ item, index }) => <TransactionItem item={item} index={index} handleClick={handleClick} />}
+          renderItem={({ item, index }) => (
+            <TransactionItem
+              item={item}
+              index={index}
+              handleClick={handleClick}
+            />
+          )}
           estimatedItemSize={60}
         />
       </View>
-      {
-        !loading && data.length == 0 && (
-          <Typo size={15} color={colors.neutral400} style={{textAlign: 'center', marginTop: spacingY._15}}>
-            {emptyListMessage}
-          </Typo>
-        )
-      }
+      {!loading && data.length == 0 && (
+        <Typo
+          size={15}
+          color={colors.neutral400}
+          style={{ textAlign: "center", marginTop: spacingY._15 }}
+        >
+          {emptyListMessage}
+        </Typo>
+      )}
+
+      {loading && (
+        <View style={{ top: verticalScale(100) }}>
+          <Loading />
+        </View>
+      )}
     </View>
   );
 };
 
 const TransactionItem = ({
-  item, index, handleClick
+  item,
+  index,
+  handleClick,
 }: TransactionItemProps) => {
-  return <View>
-    <Typo>Transaction Item</Typo>
-  </View>;
+  let category = expenseCategories["utilities"];
+  const IconComponent = category.icon;
+
+  return (
+    <Animated.View entering={FadeInDown.delay(index * 70).springify().damping(14)}>
+      <TouchableOpacity style={styles.row} onPress={() => handleClick(item)}>
+        <View style={[styles.icon, { backgroundColor: category.bgColor }]}>
+          {IconComponent && (
+            <IconComponent
+              size={verticalScale(25)}
+              weight="fill"
+              color={colors.white}
+            />
+          )}
+        </View>
+
+        <View style ={styles.categoryDesc}>
+          <Typo size={17}>{category.label}</Typo>
+          <Typo size={12} color={colors.neutral400} textProps={{numberOfLines: 1}}>Paid electricity bill</Typo>
+
+        </View>
+
+        <View style={styles.amountDate}>
+          <Typo fontWeight={500} color={colors.rose}>
+            - $55.64
+          </Typo>
+          <Typo size={13} color={colors.neutral400}>
+            30 Jun
+
+          </Typo>
+
+        </View>
+      </TouchableOpacity>
+    </Animated.View>
+  );
 };
 
 export default TransactionList;
